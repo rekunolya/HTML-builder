5be606ab8603a628912e1094c436872150195a7d
Roman <rekun.olya@gmail.com>
2022-05-23 17:19:53 +0300

    feat: add solution of task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+let fs = require('node:fs');
+
+let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
+stream.on('readable', function(){
+  let data = stream.read();
+  if(data != null) console.log(data);
+});
+stream.on('end', () => {
+  console.log('end');
+});
+stream.on('error', (err) => {
+  if(err.code == 'ENOENT') {
+    console.log('File not found');
+  } else{
+    console.error(err);
+  }
+});
+
