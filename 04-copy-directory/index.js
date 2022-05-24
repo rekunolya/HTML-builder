50d06dd6f2076abd1b354bd0028d19c97afb701d
Roman <rekun.olya@gmail.com>
2022-05-24 11:09:11 +0300

    feat: add solution to task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const folder = '04-copy-directory/files';
+const newFoder = '04-copy-directory/files-copy';
 
+fs.stat(newFoder, (err, stats) => {
+  if(err) {
+    fs.mkdir(newFoder, err => {
+      if(err) throw err;
+      console.log('The folder successfully create');
+    });
+  } else {
+    fs.readdir(newFoder, (err, files) => {
+      if(err) throw err;
+      files.forEach(file => {
+        fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+          if(err) throw err;
+          //console.log('файл успешно удален');
+        });
+      });
+    }); 
+  }
+  fs.readdir(folder, (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err => {
+        if(err) throw err;
+        //console.log('files successfully copied');
+      });
+    });
+  });
+});
